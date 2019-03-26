8cddba8b0a4234c782ad762fc3ae7b5dce3c8638
Srinath <sv072002@gmail.com>
2019-03-26 11:00:16 -0400

    signup database functionality

--- src/main/java/app/controllers/SignupController.java
+++ src/main/java/app/controllers/SignupController.java
@@ -4,6 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by Srinath on 3/25/2019.
  */
@@ -19,7 +25,18 @@ public class SignupController {
     public JFXTextField userField;
 
     @FXML
-    public void onSignup() {
-        
+    public void onSignup() throws SQLException {
+        Connection conn = null;
+        String url = "jdbc:sqlite:C:\\Users\\Srinath\\com.srinath.coding\\SoftwareDevelopment2019\\SoftwareDev.db";
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Statement statement = conn.createStatement();
+
+
+        ResultSet rs = statement.executeQuery("select * from users");
+        statement.executeUpdate("insert into users (username, password) values('" + userField.getText().toString() + "', '" + passField.getText().toString() + "')");
     }
 }
